e8cffe7f40621efbbd22384f14ae2d4c8ab58a99
DaiyuZhang <gurdjieff100@gmail.com>
2014-11-10 22:58:30 +0000

    push to github init

---
+++ app/controllers/static_pages_controller.rb
@@ -0,0 +1,15 @@
+class StaticPagesController < ApplicationController
+   def home
+      if signed_in?
+          @micropost  = current_user.microposts.build
+          @feed_items = current_user.feed
+      end
+   end
+
+  def help
+  end
+
+  def about
+          # flash[:notice] = "Testing the flash"
+      end
+end
---
+++ app/models/user.rb
@@ -0,0 +1,22 @@
+class User < ActiveRecord::Base
+    has_many :microposts     # NEW LINE - Indicates association with Micropost
+    has_many :microposts, dependent: :destroy   #  CHANGED
+    before_save do |user| 
+        user.email = email.downcase 
+	    user.remember_token = SecureRandom.urlsafe_base64
+    end
+
+	validates :name, presence: true, length: { in: 9..30 }
+    VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+    validates :email, presence: true, 
+                        format: { with: VALID_EMAIL_REGEX },
+                    uniqueness: { case_sensitive: false }
+    validates :password, presence: true, length: { minimum: 6 }
+    validates :password_confirmation, presence: true
+    has_secure_password      # A magic method!!
+
+    def feed
+        Micropost.where("user_id = ?", id)
+    end
+
+end
